e085c9c8622c8dff91faf6f5355c9881908dd11e
Daniel-Briere <daniel.briere01@gmail.com>
2022-10-10 14:45:56 -0600

    added portfolio

---
+++ Portfolio/JS/JavaScript.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if(x =="") {
+        alert("Fields must be filled out");
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
